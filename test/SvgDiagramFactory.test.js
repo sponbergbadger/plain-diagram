b3e88eeb430eadcf8b94afbf67c6db82b6bd7eac
Hans Sponberg <sponbergbadger@users.noreply.github.com>
2020-02-08 20:00:40 -0800

    convertRelativePathsToAbsolute upon request

--- test/SvgDiagramFactory.test.js
+++ test/SvgDiagramFactory.test.js
@@ -49,3 +49,28 @@ test('it should resolve absolute file paths', () => {
   expect(fs.existsSync(outputPath)).toBeTruthy()
   expect(svg.g.image._attributes.href).toBe('../images/image.svg')
 })
+
+test('it should output absolute paths upon request', () => {
+  const basePath = path.resolve(__dirname, 'assets')
+  const pluginPaths = [path.resolve(__dirname, 'assets/js/Plugins.js')]
+  const filename = 'diagram19'
+
+  const outputPath = path.resolve(basePath, 'output', filename + '.svg')
+  if (fs.existsSync(outputPath)) {
+    fs.unlinkSync(outputPath)
+  }
+
+  const diagramFactory = new SvgDiagramFactory(basePath, pluginPaths)
+  diagramFactory.convertRelativePathsToAbsolute = true
+
+  const input = path.resolve(basePath, filename + '.txt')
+  const output = path.resolve(basePath, 'output', filename + '.svg')
+
+  const xml = diagramFactory.renderDiagram(input, output)
+  const svg = XmlJS.xml2js(xml, {compact: true}).svg
+
+  const absoluteImagePath = path.resolve(__dirname, 'assets', 'images', 'image.svg')
+
+  expect(fs.existsSync(outputPath)).toBeTruthy()
+  expect(svg.g.image._attributes.href).toBe(absoluteImagePath)
+})
